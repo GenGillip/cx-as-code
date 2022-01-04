10b7c98ff9d59934dc1eb4b439b565958d035940
GenGillip <88153148+GenGillip@users.noreply.github.com>
2022-01-04 15:39:31 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 
-variable "genesys_email_domain" {
-  type        = string
-  description = "The name of the email domain  This is used to help build the email route"
-}
+# variable "genesys_email_domain" {
+#   type        = string
+#   description = "The name of the email domain  This is used to help build the email route"
+# }
 
-variable "genesys_email_domain_region" {
-  type        = string
-  description = "The name of the email region that the email domain name will reside in."
-}
+# variable "genesys_email_domain_region" {
+#   type        = string
+#   description = "The name of the email region that the email domain name will reside in."
+# }
 
-variable "classifier_url" {
-  type        = string
-  description = "The URL to call the classifier"
-}
+# variable "classifier_url" {
+#   type        = string
+#   description = "The URL to call the classifier"
+# }
 
-variable "classifier_api_key" {
-  type        = string
-  description = "API Key for classifier endpoint"
-}
+# variable "classifier_api_key" {
+#   type        = string
+#   description = "API Key for classifier endpoint"
+# }
 
 
 
