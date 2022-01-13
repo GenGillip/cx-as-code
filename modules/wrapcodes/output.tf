551129b31c35878b5c9f276432eb770857f85cb7
GenGillip <88153148+GenGillip@users.noreply.github.com>
2022-01-13 13:06:54 -0600

    Update output.tf

--- modules/wrapcodes/output.tf
+++ modules/wrapcodes/output.tf
@@ -1,4 +1,5 @@
 
 output "wrapcode_ids" {
-  value = [genesyscloud_routing_wrapupcode.wrapcodes.id]
+  # value = [genesyscloud_routing_wrapupcode.wrapcodes.id]
+  value = genesyscloud_routing_wrapupcode.wrapcodes[each.key]
 }
