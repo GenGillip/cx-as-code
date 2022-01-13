857ef054442896f0c30fe45d132060eefe654c4c
GenGillip <88153148+GenGillip@users.noreply.github.com>
2022-01-13 13:03:14 -0600

    Update main.tf

--- modules/wrapcodes/main.tf
+++ modules/wrapcodes/main.tf
@@ -12,11 +12,11 @@ terraform {
 #
 #  Description:
 #  
-#  Dynamically creates a wrapup code based on the classifier_wrapcode_name variable passed into module.  
+#  Dynamically creates a wrapup code based on the classifier_wrapcode_names variable passed into module.  
 #  This module demonstrates how Terraform can be used to create several objects using their scripting
 #  language.
 ###
 resource "genesyscloud_routing_wrapupcode" "wrapcodes" {
-  for_each                 = toset(var.classifier_wrapcode_name)
+  for_each                 = toset(var.classifier_wrapcode_names)
   name                     = each.value
 }
