ee42fa53c94f23437efb713848e622b3071459e3
GenGillip <88153148+GenGillip@users.noreply.github.com>
2022-02-25 11:04:35 -0600

    Update Group Settings

--- modules/groups/main.tf
+++ modules/groups/main.tf
@@ -13,14 +13,10 @@ resource "genesyscloud_group" "nexus_group_1" {
   name          = "Nexus Terra Group"
   description   = "Group for Testiong Terraform"
   type          = "official"
-  visibility    = "private"
+  visibility    = "members"
   rules_visible = true
   owner_ids     = [data.genesyscloud_user.jeremy_gillip.id]
   member_ids    = []
-  addresses {
-    number = ""
-    type   = ""
-  }
 }
 
 data "genesyscloud_user" "jeremy_gillip" {
