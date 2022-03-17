7367d49bb7953b1a54303e1e7777c24efd7ce56d
Jeremy Gillip <jeremy.gillip@genesys.com>
2022-03-17 10:14:46 -0500

    Revert "Update core.tf"
    This reverts commit b2130f6b373ea16a156443fabc33a25b175d865d.

--- modules/email_project/core.tf
+++ modules/email_project/core.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+###
+# Description:  
+#
+#
+# Note:  Currently we only allow a two email domain routes per Genesys Cloud organization.  You can contact CARE for an additional email route.  This
+# command will fail if there is already have two email routes present.
+###
+
 resource "genesyscloud_routing_email_route" "nexus_route" {
   domain_id    = data.genesyscloud_routing_email_domain.csp_email_domain.id
   pattern      = "nexus"
