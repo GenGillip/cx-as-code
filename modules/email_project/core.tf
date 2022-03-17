e38ad582f9ee6d3afff7a9d4121a264b31e822cf
Jeremy Gillip <jeremy.gillip@genesys.com>
2022-03-17 10:14:09 -0500

    Update core.tf

--- modules/email_project/core.tf
+++ modules/email_project/core.tf
@@ -12,20 +12,12 @@ resource "genesyscloud_routing_email_route" "nexus_route" {
   pattern      = "nexus"
   from_name    = "Nexus Support"
   from_email   = "nexus@csp.mypurecloud.com"
-  #queue_id     = genesyscloud_routing_queue.test-queue.id
-  #priority     = 5
-  #skill_ids    = [genesyscloud_routing_skill.support.id]
-  #language_id  = genesyscloud_routing_language.english.id
   flow_id      = data.genesyscloud_flow.email_flow.id
   spam_flow_id = data.genesyscloud_flow.email_flow.id
   reply_email_address {
     domain_id = data.genesyscloud_routing_email_domain.csp_email_domain.id
     route_id  = genesyscloud_routing_email_route.nexus_route.id
   }
-  # auto_bcc {
-  #   name  = "Test Support"
-  #   email = "support@test.example.com"
-  # }
 }
 
 data "genesyscloud_flow" "email_flow" {
