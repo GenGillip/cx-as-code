af6efa8d0ef1a92dde612893bbc222df82d13cab
Jeremy Gillip <jeremy.gillip@genesys.com>
2022-07-19 08:57:49 -0500

    Update main.tf

--- modules/emailproject/main.tf
+++ modules/emailproject/main.tf
@@ -64,6 +64,27 @@ resource "genesyscloud_routing_email_route" "nexustest_route" {
   # }
 }
 
+resource "genesyscloud_routing_email_route" "nexus_terra_route" {
+  domain_id    = data.genesyscloud_routing_email_domain.csp_email_domain.id
+  pattern      = "nexusterra"
+  from_name    = "Nexus Terra"
+  #from_email   = "nexusterra@csp.mypurecloud.com"
+  #queue_id     = genesyscloud_routing_queue.test-queue.id
+  #priority     = 5
+  #skill_ids    = [genesyscloud_routing_skill.support.id]
+  #language_id  = genesyscloud_routing_language.english.id
+  flow_id      = data.genesyscloud_flow.email_flow.id
+  spam_flow_id = data.genesyscloud_flow.email_flow.id
+  reply_email_address {
+    domain_id = data.genesyscloud_routing_email_domain.csp_email_domain.id
+    route_id  = "${genesyscloud_routing_email_route.aphotline-rogers-com.id}" #var.email-resource-id #genesyscloud_routing_email_route.nexus_route.id
+  }
+  # auto_bcc {
+  #   name  = "Test Support"
+  #   email = "support@test.example.com"
+  # }
+}
+
 data "genesyscloud_flow" "email_flow" {
   name = "Nexus Email Flow"
 }
